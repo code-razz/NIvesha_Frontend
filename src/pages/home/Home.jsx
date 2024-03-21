3af1a574474619e1ee952a71e00fda93f7913a9f
Raj Sah <rajsah5557@gmail.com>
2024-03-21 11:22:44 +0530

    added fake fetch data in homepage to connect to backend at homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,12 @@ import Footer from '../../components/Footer.jsx';
 import 'https://kit.fontawesome.com/cfdb111d3e.js';
 
 function Home(){
+
+    const {data,loading,error}=useFetch("https://nivesha-backend.onrender.com/api/company");
+    if(data){
+        console.log("Backend Connected Successfully");
+    }
+
     return(
         <>
         <div className="hbody">
